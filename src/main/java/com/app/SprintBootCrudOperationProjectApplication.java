8c8f521fa624ba7e52362ccbd9a0e35976c30393
ahdeielna <abdul.adil.hyd@gmail.com>
2021-12-17 20:34:39 +0530

    Initial proj Spring Boot App

---
+++ src/main/java/com/app/SprintBootCrudOperationProjectApplication.java
@@ -0,0 +1,13 @@
+package com.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SprintBootCrudOperationProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SprintBootCrudOperationProjectApplication.class, args);
+	}
+
+}
